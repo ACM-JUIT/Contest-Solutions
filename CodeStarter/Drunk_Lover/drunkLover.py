5ddf6e40256f90d7cbaeb141a2d3d90c9875b202
Dev Tyagi <56474716+devptyagi@users.noreply.github.com>
2020-10-29 13:07:14 +0530

    Update drunkLover.py

--- CodeStarter/Drunk_Lover/drunkLover.py
+++ CodeStarter/Drunk_Lover/drunkLover.py
@@ -1,10 +1,8 @@
 q = int(input())
 while(q > 0):
     a, b, t = map(int, input().split())
-    if(t % 2 == 0):
-        ans = (t // 2) * (a - b)
-    else:
-        ans = (t // 2) * (a - b)
+    ans = (t // 2) * (a - b)
+    if(t % 2 != 0):
         ans += a
     print(ans)
     q -= 1
